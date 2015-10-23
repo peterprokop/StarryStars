630cdfb71f2b75879969a4c455b52723af16a582
Peter Prokop <prokop.petr@gmail.com>
2015-10-23 10:03:43 +0300

    Initial Commit

---
+++ RateyRateExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RateyRateExample
+//
+//  Created by Peter Prokop on 23/10/15.
+//  Copyright © 2015 Peter Prokop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
